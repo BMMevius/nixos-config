65c3128c956025896506f17fd82a033bb218f811
Bastiaan Mevius <bastiaanmevius@gmail.com>
2026-03-31 08:52:50 +0200

    Add exclusion of Elisa package from Plasma 6 environment

--- modules/nixos/kde.nix
+++ modules/nixos/kde.nix
@@ -1,5 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  environment.plasma6.excludePackages = [
+    pkgs.kdePackages.elisa
+  ];
 }
