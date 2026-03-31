afa9b9fdfab24c77384e8fbefdf92cec9cdc489f
Bastiaan Mevius <bastiaanmevius@gmail.com>
2026-03-31 08:52:55 +0200

    Add xdg.mimeApps configuration for Strawberry audio player

--- modules/home-manager/strawberry.nix
+++ modules/home-manager/strawberry.nix
@@ -3,4 +3,21 @@
   home.packages = [
     pkgs.strawberry
   ];
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "audio/mpeg" = "org.strawberrymusicplayer.strawberry.desktop";
+      "audio/flac" = "org.strawberrymusicplayer.strawberry.desktop";
+      "audio/ogg" = "org.strawberrymusicplayer.strawberry.desktop";
+      "audio/wav" = "org.strawberrymusicplayer.strawberry.desktop";
+      "audio/x-wav" = "org.strawberrymusicplayer.strawberry.desktop";
+      "audio/mp4" = "org.strawberrymusicplayer.strawberry.desktop";
+      "audio/aac" = "org.strawberrymusicplayer.strawberry.desktop";
+      "audio/x-vorbis+ogg" = "org.strawberrymusicplayer.strawberry.desktop";
+      "audio/x-opus+ogg" = "org.strawberrymusicplayer.strawberry.desktop";
+      "audio/x-ms-wma" = "org.strawberrymusicplayer.strawberry.desktop";
+      "audio/x-aiff" = "org.strawberrymusicplayer.strawberry.desktop";
+    };
+  };
 }
