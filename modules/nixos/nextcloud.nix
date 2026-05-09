2bee3061e85cb13551505d7a75e1b3443f0ec82c
Bastiaan Mevius <bastiaanmevius@gmail.com>
2026-05-09 10:22:25 +0200

    Add collabora to nextcloud

--- modules/nixos/nextcloud.nix
+++ modules/nixos/nextcloud.nix
@@ -22,18 +22,21 @@ in
     appstoreEnable = false;
     extraAppsEnable = true;
     extraApps = {
-      inherit (config.services.nextcloud.package.packages.apps) memories;
+      inherit (config.services.nextcloud.package.packages.apps) memories richdocuments;
     };
     settings = {
+      allow_local_remote_servers = true;
       updatechecker = false;
       "upgrade.disable-web" = true;
       trusted_domains = [
         "desktop"
         "desktop.local"
         "192.168.1.88"
+        "localhost"
+        "::1"
+        "127.0.0.1"
       ];
       overwriteprotocol = "http";
-      "overwrite.cli.url" = "http://desktop.local";
     };
     config = {
       dbtype = "pgsql";
@@ -46,11 +49,59 @@ in
     };
   };
 
+  services.collabora-online = {
+    enable = true;
+    aliasGroups = [
+      {
+        host = "http://desktop.local";
+        aliases = [
+          "http://desktop"
+          "http://192.168.1.88"
+          "http://localhost"
+          "http://127.0.0.1"
+        ];
+      }
+    ];
+    settings = {
+      server_name = "desktop.local";
+      ssl = {
+        enable = false;
+        termination = false;
+      };
+
+      # Restrict WOPI requests to Nextcloud instance only.
+      # These are the addresses from which Nextcloud will request documents.
+      storage.wopi = {
+        "@allow" = true;
+        host = [
+          "127.0.0.1"
+          "localhost"
+          "192.168.1.88"
+          "desktop"
+          "desktop.local"
+        ];
+      };
+    };
+  };
+
   systemd.services.nextcloud-setup = {
     postStart = ''
       ${config.services.nextcloud.occ}/bin/nextcloud-occ config:app:set \
         memories maps_tile_server \
         --value="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ config:app:set \
+        richdocuments wopi_url \
+        --value="http://desktop.local"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ config:app:set \
+        richdocuments public_wopi_url \
+        --value="http://desktop.local"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ config:app:set \
+        richdocuments wopi_allowlist \
+        --value="127.0.0.1,::1,192.168.1.88"
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ config:app:delete \
+        richdocuments wopi_callback_url || true
+      ${config.services.nextcloud.occ}/bin/nextcloud-occ config:app:delete \
+        richdocuments disable_certificate_verification || true
     '';
   };
 
@@ -82,6 +133,54 @@ in
       "zfs-mount.service"
     ];
     unitConfig.RequiresMountsFor = [ storageMountPoint ];
+  };
+
+  services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+    serverAliases = [
+      "desktop"
+      "localhost"
+      "127.0.0.1"
+      "192.168.1.88"
+    ];
+    locations = {
+      "^~ /browser" = {
+        proxyPass = "http://127.0.0.1:9980";
+        recommendedProxySettings = true;
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Forwarded-Host $host;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+      "= /hosting/discovery" = {
+        proxyPass = "http://127.0.0.1:9980/hosting/discovery";
+        recommendedProxySettings = true;
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Forwarded-Host $host;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+      "= /hosting/capabilities" = {
+        proxyPass = "http://127.0.0.1:9980/hosting/capabilities";
+        recommendedProxySettings = true;
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Forwarded-Host $host;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+      "^~ /cool" = {
+        proxyPass = "http://127.0.0.1:9980";
+        proxyWebsockets = true;
+        recommendedProxySettings = true;
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Forwarded-Host $host;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+    };
   };
 
   systemd.services."phpfpm-nextcloud".unitConfig.RequiresMountsFor = [ storageMountPoint ];
