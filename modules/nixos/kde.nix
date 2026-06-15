3f1e581230d1a9284c4e274ae6fa565b36e68556
Bastiaan Mevius <bastiaan.mevius@sorama.eu>
2026-06-15 13:36:30 +0200

    Add polkit rules to allow wheel group users to modify NetworkManager connections

--- modules/nixos/kde.nix
+++ modules/nixos/kde.nix
@@ -6,6 +6,26 @@
   services.udisks2.enable = true;
   security.polkit.enable = true;
 
+  # Allow wheel group users to modify NetworkManager connections without password prompts.
+  # Uses two complementary rules: one for permission grants, one to handle secrets fetching.
+  environment.etc."polkit-1/rules.d/49-networkmanager-wheel.rules".text = ''
+    polkit.addRule(function(action, subject) {
+      if (action.id.indexOf("org.freedesktop.NetworkManager.") == 0 && 
+          subject.isInGroup("wheel")) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
+  environment.etc."polkit-1/rules.d/50-networkmanager-secrets.rules".text = ''
+    polkit.addRule(function(action, subject) {
+      if (action.id == "org.freedesktop.NetworkManager.settings.modify.hostname" &&
+          subject.isInGroup("wheel")) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
   environment.systemPackages = with pkgs; [
     kdePackages.kwalletmanager
     kdePackages.kwallet-pam
